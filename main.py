9484b7ea724de8e8495c7c92aad97b104d47eba9
Y Ho <yho@Ys-iMac.local>
2026-04-25 10:47:43 -0700

    initial project setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works in a few words"
+)
+
+print(response.text)
